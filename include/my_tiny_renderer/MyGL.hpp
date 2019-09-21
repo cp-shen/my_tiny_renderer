9fdbc7ccbaa3672bcc350eb80a5918266195caca
scp <cp_shen@outlook.com>
2019-09-21 18:19:31 +0800

    updated

--- include/my_tiny_renderer/MyGL.hpp
+++ include/my_tiny_renderer/MyGL.hpp
@@ -1,12 +1,34 @@
 #ifndef MYGL_H
 #define MYGL_H
 
+#include <png++/png.hpp>
+
+/**
+ * \brief conataining static methods to draw
+ */
 class MyGL {
 private:
 public:
-    MyGL ();
-    virtual ~MyGL ();
-    static void DrawLine();
+    MyGL();
+    virtual ~MyGL();
+    /**
+     * \brief
+     * \param[in]  x0
+     * \param[in]  y0
+     * \param[in]  x1
+     * \param[in]  y1
+     * \param[in]  image
+     * \param[in]  color
+     * \exception
+     */
+    static void DrawLine(
+            int x0,
+            int y0,
+            int x1,
+            int y1,
+            png::image<png::rgb_pixel>& image,
+            png::rgb_pixel& color
+            );
 };
 
 #endif /* MYGL_H */
--- src/MyGL.cpp
+++ src/MyGL.cpp
@@ -1,4 +1,13 @@
 #include <my_tiny_renderer/MyGL.hpp>
 
-void MyGL::DrawLine()
-{ }
+void MyGL::DrawLine(
+            int x0,
+            int y0,
+            int x1,
+            int y1,
+            png::image<png::rgb_pixel>& image,
+            png::rgb_pixel& color)
+{
+    int deltaX = std::abs(x0 - x1);
+    int deltaY = std::abs(y0 - y1);
+}
