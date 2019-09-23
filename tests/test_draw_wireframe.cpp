26c037ed382031cd688810fdcebfb485d37b8816
scp <cp_shen@outlook.com>
2019-09-23 20:23:06 +0800

    fix pixel out of border bug

--- tests/test_draw_wireframe.cpp
+++ tests/test_draw_wireframe.cpp
@@ -53,16 +53,16 @@ TEST_CASE("Draw Wireframe")
                 tinyobj::real_t vx1 = attrib.vertices[3 * idx_next.vertex_index + 0];
                 tinyobj::real_t vy1 = attrib.vertices[3 * idx_next.vertex_index + 1];
 
-                int x0 = (vx0 + 1.) * image_width / 2.;
-                int y0 = (vy0 + 1.) * image_height / 2.;
-                int x1 = (vx1 + 1.) * image_width / 2.;
-                int y1 = (vy1 + 1.) * image_height / 2.;
+                int x0 = (vx0 + 1.) * ( image_width / 2. - 1.);
+                int y0 = (vy0 + 1.) * ( image_height / 2. - 1.);
+                int x1 = (vx1 + 1.) * ( image_width / 2. - 1.);
+                int y1 = (vy1 + 1.) * ( image_height / 2. - 1.);
 
-                REQUIRE((x0 >= 0 && x0 < image_width));
-                REQUIRE((y0 >= 0 && y0 < image_height));
+                REQUIRE(x0 >= 0); REQUIRE(x0 < image_width);
+                REQUIRE(y0 >= 0); REQUIRE(y0 < image_height);
 
-                REQUIRE((x1 >= 0 && x1 < image_width));
-                REQUIRE((y1 >= 0 && y1 < image_height));
+                REQUIRE(x1 >= 0); REQUIRE(x1 < image_width);
+                REQUIRE(y1 >= 0); REQUIRE(y1 < image_height);
 
                 // Draw Wireframe
                 REQUIRE_NOTHROW(MyGL::DrawLine(x0, y0, x1, y1, image, pixel_white));
