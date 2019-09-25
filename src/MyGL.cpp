a607e311c2cb6702bb2772fe15cf455171decb22
scp <cp_shen@outlook.com>
2019-09-25 10:34:27 +0800

    rewrite draw line

--- src/MyGL.cpp
+++ src/MyGL.cpp
@@ -10,15 +10,11 @@ void MyGL::DrawLine (
             png::image<png::rgb_pixel>& image,
             png::rgb_pixel& color)
 {
-    int image_width = image.get_width();
-    int image_height = image.get_height();
-    Box box = Box(image_width, image_height);
-
-    bool transposed = std::abs(x1 - x0) < std::abs(y1 - y0);
-
-    if (transposed) {
+    bool steep = false;
+    if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
         std::swap(x0, y0);
         std::swap(x1, y1);
+        steep = true;
     }
 
     if (x0 > x1) {
@@ -26,52 +22,23 @@ void MyGL::DrawLine (
         std::swap(y0, y1);
     }
 
-    int deltaX = x1 - x0;
-    int deltaY = y1 - y0;
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    int derror = std::abs(dy) * 2;
+    int error = 0;
+    int y = y0;
 
-    // only one point in the line
-    if (x0 == x1) {
-        image.set_pixel(x0, y0, color);
-        return;
-    }
+    for (int x = x0; x <= x1; x++) {
+        if (steep)
+            image.set_pixel(y, x, color);
+        else
+            image.set_pixel(x, y, color);
 
-    try {
-        int y = y0;
-        float error = 0;
-        float deltaError = std::abs(deltaY / (float)deltaX);
-        for (int x = x0; x <= x1; x++) {
-            if (transposed) {
-                if(box.CheckPointInBox(glm::vec2(y, x)))
-                    image.set_pixel(y, x, color);
-                else
-                    throw std::runtime_error("draw failure: "
-                            + std::to_string(y) + ", " + std::to_string(x) + ", " + std::to_string(transposed));
-            }
-            else {
-                if(box.CheckPointInBox(glm::vec2(x, y)))
-                    image.set_pixel(x, y, color);
-                else
-                    throw std::runtime_error("draw failure: "
-                            + std::to_string(x) + ", " + std::to_string(y) + ", " + std::to_string(transposed));
-            }
-            error += deltaError;
-            if (error >= 0.5) {
-                y += (deltaY > 0 ? 1 : -1);
-                error -= 1;
-            }
+        error += derror;
+        if (error >= dx) {
+            y += (dy > 0 ? 1 : -1);
+            error -= dx * 2;
         }
-    }
-    catch (const std::exception& e) {
-        std::stringstream errStr;
-        errStr << e.what() << std::endl;
-
-        errStr << " x0=" + std::to_string(x0);
-        errStr << " y0=" + std::to_string(y0);
-        errStr << " x1=" + std::to_string(x1);
-        errStr << " y1=" + std::to_string(y1);
-
-        std::runtime_error err(errStr.str());
-        throw err;
     }
 }
 
